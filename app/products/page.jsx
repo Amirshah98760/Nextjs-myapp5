88210fc8347b85b6435fb1f3b6db796cfec4a483
Amirshah98760 <amirshah98760@gmail.com>
2026-02-20 12:13:13 +0500

    I learn the real world uses of searchparams in nextjs and implement

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
+// import React from 'react'
 
-const  Products = async ({searchParams}) => {
-    // const searchParams = await props.searchParams;
-    // console.log(searchParams);
-const resolvedSearchParams  = await searchParams;
+// const  Products = async ({searchParams}) => {
+//     // const searchParams = await props.searchParams;
+//     // console.log(searchParams);
+// const resolvedSearchParams  = await searchParams;
 
-    const category = resolvedSearchParams?.category || "all";
-    const sort = resolvedSearchParams?.sort || "asc";
-    const page = resolvedSearchParams?.page || 1;
+//     const category = resolvedSearchParams?.category || "all";
+//     const sort = resolvedSearchParams?.sort || "asc";
+//     const page = resolvedSearchParams?.page || 1;
+
+//   return (
+//    <div>
+
+//     <h1>Products Page</h1>
+//     <p>Category: {category}</p>
+//     <p>Sort: {sort}</p>
+//     <p>Page: {page}</p>
+//    </div>
+   
+// )
+// }
+
+// export default Products
+
+
+
+
+// Real World use case of search params in Next.js 13 app directory
+
+const Products = async ({ searchParams }) => {
+
+  const { category = "all", sort = "asc", page = 1 } = await searchParams;
+
+  const res = await fetch(
+    `https://dummyjson.com/products/category/${category}?limit=10&skip=${(page - 1) * 10}`,
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+//   console.log(data)
+
+  let products = data.products;
+
+  // sort frontend (if API doesn't support)
+  products = products.sort((a, b) => 
+    sort === "asc" ? a.price - b.price : b.price - a.price
+  );
 
   return (
-   <div>
+    <div>
+      <h1>Products</h1>
 
-    <h1>Products Page</h1>
-    <p>Category: {category}</p>
-    <p>Sort: {sort}</p>
-    <p>Page: {page}</p>
-   </div>
-   
-)
-}
+      {products.map(p => (
+        <div key={p.id}>
+          {p.title} - ${p.price}
+        </div>
+      ))}
+
+    </div>
+  );
+};
 
 export default Products
